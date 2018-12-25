971cc110f7769adaaadbc66cd50b8dc0b375741b
Aleksa Stojanović <afidelo@protonmail.com>
2018-12-25 20:34:02 +0100

    replaced source

--- sublime.sh
+++ sublime.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-
+sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+curl -fsSL https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+sudo add-apt-repository "deb https://download.sublimetext.com/ apt/stable/"
+sudo apt update && sudo apt install sublime-text -y
